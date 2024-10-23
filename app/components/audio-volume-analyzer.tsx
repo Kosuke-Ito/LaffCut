1b946da52058bfb2ee511ffed035a75ead5423d1
Kosuke Ito <keree.it.owl.uk@gmail.com>
2024-10-24 00:46:18 +0900

    デザイン変更

--- app/components/audio-volume-analyzer.tsx
+++ app/components/audio-volume-analyzer.tsx
@@ -113,7 +113,7 @@ export function AudioVolumeAnalyzerComponent() {
   }, [file])
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <div className="flex flex-col items-center justify-center min-h-scree p-4">
       <div
         className="w-full max-w-md p-6 bg-white rounded-lg shadow-md"
         onDrop={onDrop}

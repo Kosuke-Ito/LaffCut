4f0ce4243b99fa9ee4ea19abca6c58685ee7595e
Kosuke Ito <keree.it.owl.uk@gmail.com>
2024-11-23 22:27:19 +0900

    ABORT_DELAYを削除
    remix v2からreact router v7にアプデ

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -12,8 +12,6 @@ import { RemixServer } from '@remix-run/react'
 import { isbot } from 'isbot'
 import { renderToPipeableStream } from 'react-dom/server'
 
-const ABORT_DELAY = 5_000
-
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
@@ -48,11 +46,7 @@ function handleBotRequest(
   return new Promise((resolve, reject) => {
     let shellRendered = false
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
+      <RemixServer context={remixContext} url={request.url} />,
       {
         onAllReady() {
           shellRendered = true

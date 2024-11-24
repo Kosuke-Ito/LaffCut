946c6f62b3652468cc6b6e450b09815db70d4496
Kosuke Ito <keree.it.owl.uk@gmail.com>
2024-11-24 15:05:34 +0900

    future flagを削除
    remix v2からreact router v7にアプデ

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
-import { reactRouter } from '@react-router/dev/vite';
+import { reactRouter } from '@react-router/dev/vite'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-declare module '@remix-run/node' {
-  // or cloudflare, deno, etc.
-  interface Future {
-    v3_singleFetch: true
-  }
-}
-
 export default defineConfig({
-  plugins: [
-    reactRouter({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-        v3_singleFetch: true,
-        unstable_optimizeDeps: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [reactRouter(), tsconfigPaths()],
 })

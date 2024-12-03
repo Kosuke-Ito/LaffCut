213889dd718bb320656811513ee783a35552385a
Kosuke Ito <keree.it.owl.uk@gmail.com>
2024-12-04 01:34:45 +0900

    streamTimeoutを書き忘れてサーバーによるレンダリングが理由もなく中断されてしまった

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,6 +3,7 @@
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.server
  */
+export const streamTimeout = 5000
 
 import { PassThrough } from 'node:stream'
 
@@ -79,7 +80,7 @@ function handleBotRequest(
       }
     )
 
-    setTimeout(abort)
+    setTimeout(abort, streamTimeout + 1000)
   })
 }
 
@@ -125,6 +126,6 @@ function handleBrowserRequest(
       }
     )
 
-    setTimeout(abort)
+    setTimeout(abort, streamTimeout + 1000)
   })
 }
